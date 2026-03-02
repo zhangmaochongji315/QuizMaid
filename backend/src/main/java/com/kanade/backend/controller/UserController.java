4a83f7816ea52628a5bad9b2b9d546071ee2b84c
kanade <jrdrywnt.163.com>
2026-03-02 20:53:37 +0800

    check dev

--- backend/src/main/java/com/kanade/backend/controller/UserController.java
+++ backend/src/main/java/com/kanade/backend/controller/UserController.java
@@ -14,7 +14,7 @@ public class UserController {
 
     @GetMapping("/")
     public BaseResponse<String> healthCheck() {
-        return ResultUtils.success("ok");
+        return ResultUtils.success("ok test check");
     }
 }
 
